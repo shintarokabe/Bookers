1ac8340550996e5d6db5f4388dd88f4fe3bc2406
shintarokabe <kabe0852@gmail.com>
2021-02-13 13:00:21 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,28 @@ class BooksController < ApplicationController
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def check
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+    end
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
